ffaaa6ee77340f1f317d866737d963bb87e1fde8
Thadchayini Pathmanathan <thadchayini95@gmail.com>
2018-03-23 21:04:13 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,20 +2,20 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '112070469471620', // your App ID
-        'clientSecret'  : '1ce99e757bd512e7c927b560ed5f1074', // your App Secret
+        'clientID'      : '000000000000000', // your App ID
+        'clientSecret'  : '0000000000000000000000000000000', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'ofXuiZXfVcrNPfts0J2d9Au4R',
-        'consumerSecret'    : 'lVx5BgLojobVYtXxGkwURdq2NxHudVPYUciSSEkSwwvEURmuwd',
+        'consumerKey'       : '0000000000000000000000000',
+        'consumerSecret'    : '0000000000000000000000000000000000000000',
         'callbackURL'       : 'http://localhost:127.0.0.1:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '872129927716-60pbp2vfdgj15jabqttljh04tcffb5ec.apps.googleusercontent.com',
-        'clientSecret'  : 'aOzYY-2xuhsjDEVGpkQC3ojQ',
+        'clientID'      : '000000000000-000000000000000000000000000000000.apps.googleusercontent.com',
+        'clientSecret'  : '00000-000000000000000000',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
